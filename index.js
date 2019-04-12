df4e959ce2d573e2f3b1d3750940b378ece5e12d
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-12 18:59:29 +0100

    added model

--- index.js
+++ index.js
@@ -16,11 +16,25 @@ mongoose.connect('mongodb://localhost/'+dbname)
     .then(()=>console.log('Connected to MongoDB...'))
     .catch(err=>console.error('Error connecting',err));
 
-
+//schema
 const courseSchema=new mongoose.Schema({
     name:String,
     author:String,
     tags:[String],
     date:{type:Date,default:Date.now},
     isPublished:Boolean
+});
+/**
+ * compile the schema to model which gives us a class
+ * create obj based on that class which maps to a document on the mongoDB
+ */
+
+//singular name pascal case for classes
+const Course=mongoose.model('Course',courseSchema);
+//camel case for objects
+const course=new Course({
+    name:'NodeJS Course',
+    author:'Mosh',
+    tags:['node','backend'],
+    isPublished:true
 });
