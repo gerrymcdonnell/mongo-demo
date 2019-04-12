14ddcedbb7ffd59606c51a75877fee823d46393c
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-12 18:53:09 +0100

    added course schema

--- index.js
+++ index.js
@@ -5,11 +5,22 @@
  * second package for mongoDb
  * npm i mongoose@5.0.1
  */
+//vid 86
 const mongoose=require('mongoose');
 
+//database name
 const dbname='playground';
 
 //mongoose returns a promise
 mongoose.connect('mongodb://localhost/'+dbname)
     .then(()=>console.log('Connected to MongoDB...'))
-    .catch(err=>console.error('Error connecting',err))
+    .catch(err=>console.error('Error connecting',err));
+
+
+const courseSchema=new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type:Date,default:Date.now},
+    isPublished:Boolean
+});
